7ee78a05488410b2367c1d4be827af0708591e63
Camilla <camilla.valerius@gmail.com>
2019-09-10 11:48:26 +0200

    initial entity class

---
+++ src/main/java/entities/Members.java
@@ -0,0 +1,113 @@
+package entities;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Camilla
+ */
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "Member.getAll", query = "SELECT m FROM Member m"),
+    @NamedQuery(name = "Member.getByName", query = "SELECT m FROM Member m WHERE m.name LIKE :name"),
+})
+public class Members implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String studentID;
+    private String name;
+    private String github;
+    private double rating;
+
+    public Members(String studentID, String name, String github, double rating) {
+        this.studentID = studentID;
+        this.name = name;
+        this.github = github;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.id);
+        hash = 71 * hash + Objects.hashCode(this.studentID);
+        hash = 71 * hash + Objects.hashCode(this.name);
+        hash = 71 * hash + Objects.hashCode(this.github);
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.rating) ^ (Double.doubleToLongBits(this.rating) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Members other = (Members) obj;
+        if (Double.doubleToLongBits(this.rating) != Double.doubleToLongBits(other.rating)) {
+            return false;
+        }
+        if (!Objects.equals(this.studentID, other.studentID)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.github, other.github)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+    
