88a69e3187bfe27dec15228e47fee89ebb9396c6
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-09-10 17:43:44 +0200

    Clean up

--- src/main/java/facades/WhoDidWhatFacade.java
+++ src/main/java/facades/WhoDidWhatFacade.java
@@ -86,109 +86,12 @@ public class WhoDidWhatFacade {
                 em.getTransaction().commit();
                 return student;
             }
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            throw new IllegalArgumentException("Wrong input when inputting work or name.");
         } finally {
             em.close();
         }
     }
 
-//
-//    /**
-//     * Get Ticket Sales for a Movie by ID. To be used internally only.
-//     *
-//     * @param id of the Movie.
-//     * @return Number of Ticket sales as a Long.
-//     */
-//    public Long getTicketSales(int id) {
-//        EntityManager em = getEntityManager();
-//        try {
-//            return (Long) em.createQuery("SELECT r.ticketsSold FROM Movie r WHERE r.id = :id").setParameter("id", id).getSingleResult();
-//        } finally {
-//            em.close();
-//        }
-//    }
-//
-//    /**
-//     * Get all Movies in DTO format.
-//     *
-//     * @return
-//     */
-//    public List<MovieDTO> getAllMoviesDTO() {
-//        EntityManager em = getEntityManager();
-//        try {
-//            List<Movie> movies = em.createNamedQuery("Movie.findAll").getResultList();
-//            List<MovieDTO> result = new ArrayList<>();
-//            movies.forEach((movie) -> {
-//                result.add(new MovieDTO(movie));
-//            });
-//            return result;
-//        } finally {
-//            em.close();
-//        }
-//    }
-//
-//    /**
-//     * Get MovieDTO by ID.
-//     *
-//     * @param id of the Movie
-//     * @return MovieDTO
-//     */
-//    public MovieDTO getMovieDTOById(Long id) {
-//        EntityManager em = getEntityManager();
-//        try {
-//            Movie movie = em.find(Movie.class, id);
-//            return new MovieDTO(movie);
-//        } finally {
-//            em.close();
-//        }
-//
-//    }
-//
-//    public Movie makeMovie(Movie movie) {
-//        EntityManager em = getEntityManager();
-//        try {
-//            em.getTransaction().begin();
-//            em.persist(movie);
-//            em.getTransaction().commit();
-//            return movie;
-//        } catch (Exception e) {
-//            em.getTransaction().rollback();
-//        } finally {
-//            em.close();
-//        }
-//        return null;
-//    }
-//
-//    /**
-//     * Get number of movies in the database.
-//     *
-//     * @return
-//     */
-//    public Long getMovieCount() {
-//        EntityManager em = getEntityManager();
-//        try {
-//            Long result = (long) em.createNamedQuery("Movie.count").getSingleResult();
-//            return result;
-//        } finally {
-//            em.close();
-//        }
-//    }
-//
-//    /**
-//     * Returns MovieDTO by name.
-//     *
-//     * @param name
-//     * @return
-//     * @throws java.lang.Exception
-//     */
-//    public MovieDTO getMovieDTOByName(String name) throws Exception {
-//        EntityManager em = getEntityManager();
-//        try {
-//            return em.createQuery("SELECT new dto.MovieDTO(m) FROM Movie m WHERE m.name = :name", MovieDTO.class).setParameter("name", name).getSingleResult();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            throw new Exception("No customer found by that name");
-//        } finally {
-//            em.close();
-//        }
-//    }
 }
