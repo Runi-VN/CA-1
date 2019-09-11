60f5710a17cc1d0990b9c6464c3f5edc3560f91c
Camilla <camilla.valerius@gmail.com>
2019-09-11 13:42:05 +0200

    changed data to real people

--- src/main/java/rest/StudentRessource.java
+++ src/main/java/rest/StudentRessource.java
@@ -38,11 +38,10 @@ public class StudentRessource {
         em.getTransaction().commit();
 
         ArrayList<Student> allStudents = new ArrayList();
-        allStudents.add(new Student("abc-123", "Ulrikke Jensen", "www.github.com/ulrikke"));
-        allStudents.add(new Student("bcd-234", "Orla Hansen", "www.github.com/orla"));
-        allStudents.add(new Student("cde-345", "Werner Bo", "www.github.com/werner"));
-        allStudents.add(new Student("def-456", "Gerda Sørensen", "www.github.com/gerda"));
-        allStudents.add(new Student("efg-567", "Rigmor Alfsen", "www.github.com/rigmor"));
+        allStudents.add(new Student("cs340", "Camilla Staunstrup", "https://github.com/Castau"));
+        allStudents.add(new Student("mh748", "Malte Hviid-Magnussen", "https://github.com/MalteMagnussen"));
+        allStudents.add(new Student("ab363", "Asger Bjarup", "https://github.com/HrBjarup"));
+        allStudents.add(new Student("rn118", "Rúni Niclassen", "https://github.com/Runi-VN"));
         try {
             for (Student s : allStudents) {
                 em.getTransaction().begin();
