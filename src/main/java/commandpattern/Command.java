fb268b2cb83568f3cca98337c1ea36f8da3e4783
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-09-12 12:16:12 +0200

    WhoDidWhat FrontEnd

---
+++ src/main/java/commandpattern/Command.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package commandpattern;
+
+import facades.WhoDidWhatFacade;
+import java.util.HashMap;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Malte
+ */
+public abstract class Command {
+    private static HashMap<String, Command> commands;
+
+    private static void initCommands()
+    {
+        commands = new HashMap<>();
+        commands.put("makeWork", new MakeWorkCommand()); // Insert work into the database.
+    }
+
+    public static Command from(HttpServletRequest request)
+    {
+        String commandName = request.getParameter("command");
+        if (commands == null)
+        {
+            initCommands();
+        }
+        return commands.get(commandName);
+    }
+
+    public abstract String execute(HttpServletRequest request, WhoDidWhatFacade logic);
+}
