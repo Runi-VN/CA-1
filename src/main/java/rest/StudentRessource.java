8f452af951ae3ccc764f9d230bac059b1b40f632
Camilla <camilla.valerius@gmail.com>
2019-09-12 08:59:31 +0200

    initial setup for rest test

--- src/main/java/rest/StudentRessource.java
+++ src/main/java/rest/StudentRessource.java
@@ -2,7 +2,6 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
 import entities.Student;
 import facades.StudentFacade;
 import java.util.ArrayList;
---
+++ src/test/java/rest/StudentRessourceTest.java
@@ -0,0 +1,146 @@
+package rest;
+
+import dto.JokeDTO;
+import entities.Student;
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.ws.rs.core.UriBuilder;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import utils.EMF_Creator;
+
+/**
+ *
+ * @author Camilla
+ */
+public class StudentRessourceTest {
+
+    private static final int SERVER_PORT = 7777;
+    private static final String SERVER_URL = "http://localhost/api";
+
+    static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
+    private static HttpServer httpServer;
+    private static EntityManagerFactory emf;
+    List<Joke> jokes;
+    List<JokeDTO> jokesDTO;
+
+    static HttpServer startServer() {
+        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
+        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        emf = EMF_Creator.createEntityManagerFactory(EMF_Creator.DbSelector.TEST, EMF_Creator.Strategy.CREATE);
+        httpServer = startServer();
+
+        RestAssured.baseURI = SERVER_URL;
+        RestAssured.port = SERVER_PORT;
+
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+    @AfterAll
+    public static void closeTestServer() {
+        httpServer.shutdownNow();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        Query query = em.createNativeQuery("truncate table startcode_test.MOVIE;");
+        query.executeUpdate();
+        em.getTransaction().commit();
+
+        ArrayList<Student> allStudents = new ArrayList();
+        allStudents.add(new Student("abc-123", "Ulrikke Jensen", "www.github.com/ulrikke"));
+        allStudents.add(new Student("bcd-234", "Orla Hansen", "www.github.com/orla"));
+        allStudents.add(new Student("cde-345", "Werner Bo", "www.github.com/werner"));
+        allStudents.add(new Student("def-456", "Gerda Sørensen", "www.github.com/gerda"));
+        allStudents.add(new Student("efg-567", "Rigmor Alfsen", "www.github.com/rigmor"));
+        try {
+            for (Student s : allStudents) {
+                em.getTransaction().begin();
+                em.persist(s);
+                em.getTransaction().commit();
+            }
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    @Test
+    public void testData() {
+        System.out.println("data");
+        StudentRessource instance = new StudentRessource();
+        String expResult = "";
+        String result = instance.data();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testGetAllStudents() throws Exception {
+        System.out.println("getAllStudents");
+        StudentRessource instance = new StudentRessource();
+        String expResult = "";
+        String result = instance.getAllStudents();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testGetStudentDTOByStudentID() throws Exception {
+        System.out.println("getStudentDTOByStudentID");
+        String studentID = "";
+        StudentRessource instance = new StudentRessource();
+        String expResult = "";
+        String result = instance.getStudentDTOByStudentID(studentID);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testGetStudentByID() throws Exception {
+        System.out.println("getStudentByID");
+        long id = 0L;
+        StudentRessource instance = new StudentRessource();
+        String expResult = "";
+        String result = instance.getStudentByID(id);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testGetStudentDTOByName() throws Exception {
+        System.out.println("getStudentDTOByName");
+        String name = "";
+        StudentRessource instance = new StudentRessource();
+        String expResult = "";
+        String result = instance.getStudentDTOByName(name);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+}
