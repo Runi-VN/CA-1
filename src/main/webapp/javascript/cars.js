757e48442cf2f60955efc054374a9f220393ef54
HrBjarup <cph-ab363@cphbusiness.dk>
2019-09-15 16:37:02 +0200

    Base before coding sorting code

--- src/main/webapp/javascript/cars.js
+++ src/main/webapp/javascript/cars.js
@@ -12,115 +12,44 @@ var buttonAll = document.getElementById("button-all");
 
 var buttonID = document.getElementById("button-id");
 
-var buttonSortPrice = document.getElementById("button-price-sort");
-
-var buttonSortYear = document.getElementById("button-year-sort");
-
-var buttonSortMake = document.getElementById("button-make-sort");
-
-
 //Eventlisteners and functions
-
-var sortCounter = 0;
-
-buttonSortPrice.addEventListener("click", function () {
-    var price = document.getElementById("input-price").value;
-    let url = "/CA-1/api/car/all";
-    if (isNaN(price) || price == "")
-    {
-        getCars(url, null, null, sortPrice);
-        return;
-    }
-    if (sortCounter === 0)
-    {
-        getCars(url, filterPrice, price, sortPrice);
-    } else
-    {
-        getCars(url, filterPrice, price, sortPrice);
-    }
-});
-
-buttonSortYear.addEventListener("click", function () {
-    var year = document.getElementById("input-year").value;
-    let url = "/CA-1/api/car/all";
-    if (isNaN(year) || year == "")
-    {
-        getCars(url, null, null, sortYear);
-        return;
-    }
-    if (sortCounter === 0)
-    {
-        getCars(url, filterYear, year, sortYear);
-    } else
-    {
-        getCars(url, filterYear, year, sortYear);
-    }
-});
-
-buttonSortMake.addEventListener("click", function () {
-    var make = document.getElementById("input-make").value;
-    let url = "/CA-1/api/car/all";
-    if (make == "")
-    {
-        getCars(url, null, null, sortMake);
-        return;
-    }
-    if (sortCounter === 0)
-    {
-        getCars(url, filterMake, make, sortMake);
-    } else
-    {
-        getCars(url, filterMake, make, sortMake);
-    }
-});
 
 buttonAll.addEventListener("click", function () {
     let url = "/CA-1/api/car/all";
     getCars(url);
-    sortCounter = 0;
 });
 
 buttonID.addEventListener("click", function () {
     var ID = document.getElementById("input-id").value;
     let url = "/CA-1/api/car/"+ID+"/allInfo";
     getCarByID(url);
-    sortCounter = 0;
 });
 
 buttonPrice.addEventListener("click", function () {
     var price = document.getElementById("input-price").value;
     let url = "/CA-1/api/car/all";
     getCars(url, filterPrice, price);
-    sortCounter = 0;
 });
 
 buttonYear.addEventListener("click", function () {
     var year = document.getElementById("input-year").value;
     let url = "/CA-1/api/car/all";
     getCars(url, filterYear, year);
-    sortCounter = 0;
 });
 
 buttonMake.addEventListener("click", function () {
     var make = document.getElementById("input-make").value;
     let url = "/CA-1/api/car/all";
     getCars(url, filterMake, make);
-    sortCounter = 0;
 });
 
 function filterMake(array, filter){return array.filter( car => car.Make.toLowerCase() === filter.toLowerCase() )};
 
-function sortMake(array){return array.sort( (a, b) => a.Make.toUpperCase().localeCompare(b.Make.toUpperCase()) );};
-
 function filterYear(array, filter){return array.filter( car => car.Year >= filter )};
-
-function sortYear(array){return array.sort( (a, b) => a.Year < b.Year );};
 
 function filterPrice(array, filter){return array.filter( car => car.Price <= filter )};
 
-function sortPrice(array){return array.sort( (a, b) => a.Price < b.Price );};
-
-function getCars(url, callback, filter, callback2) {
+function getCars(url, callback, filter) {
     fetch(url)
     .then(res => res.json()) //in flow1, just do it
     .then(data => {
@@ -132,18 +61,10 @@ function getCars(url, callback, filter, callback2) {
             var temp = callback(arrayOfObj, filter);
             arrayOfObj = temp;
         }
-        if (typeof callback2 === "function")
+        if (typeof arrayOfObj[0] === "undefined")
         {
-            if (sortCounter === 0)
-            {
-                arrayOfObj = callback2(arrayOfObj);
-                sortCounter = 1;
-            } else
-            {
-                arrayOfObj = callback2(arrayOfObj);
-                arrayOfObj = arrayOfObj.reverse();
-                sortCounter = 0;
-            }
+            outputDiv.innerHTML = "";
+            return;
         }
         // Make Table Header
         HTML = tableHeader(Object.keys(arrayOfObj[0]));
